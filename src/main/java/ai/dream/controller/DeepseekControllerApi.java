9c626d2105a10a2490e88ddb231f2b2871e92606
ayaka <2521845542@qq.com>
2025-03-12 11:01:44 +0800

    deepseek api对接 docker

--- src/main/java/ai/dream/controller/DeepseekControllerApi.java
+++ src/main/java/ai/dream/controller/DeepseekControllerApi.java
@@ -8,6 +8,8 @@ import ai.dream.httpResponse.BalanceResp;
 import ai.dream.httpResponse.ChatAiResp;
 import ai.dream.httpResponse.CompletionsResp;
 import ai.dream.service.DeepseekService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
@@ -24,6 +26,7 @@ public class DeepseekControllerApi {
      * 对话补全
      */
     @PostMapping("/chat")
+    @Operation(summary = "对话", description = "对话")
     public ResultBody<ChatAiResp> chat(@RequestBody @Validated ReqBody<ChatAiReq> req) {
         return ResultBody.ok(deepseekService.chat(req.getData()));
     }
@@ -32,6 +35,7 @@ public class DeepseekControllerApi {
      * fim补全
      */
     @PostMapping("/completions")
+    @Operation(summary = "场景补全", description = "通过输入的场景补全后续可能发生的事件")
     public ResultBody<CompletionsResp> completions(@RequestBody @Validated ReqBody<CompletionsReq> req) {
         return ResultBody.ok(deepseekService.completions(req.getData()));
     }
@@ -40,6 +44,7 @@ public class DeepseekControllerApi {
      * 余额查询
      */
     @GetMapping("/balance")
+    @Operation(summary = "余额查询", description = "查询我充的钱还剩多少")
     public ResultBody<BalanceResp> balance() {
         return ResultBody.ok(deepseekService.balance());
     }
